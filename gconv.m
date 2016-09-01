8ebcc7fff4fbb45805e1bb431aa1e4210ccd7f18
xin-yi <xinyi@math.bu.edu>
2016-09-01 11:11:33 -0400

    Update gconv.m

--- gconv.m
+++ gconv.m
@@ -7,9 +7,8 @@ expg_k=zeros(ntime,ntrial);
 
 for tr=1:ntrial
     if isempty(find(trainM(:,tr)))==1
-        t_elapse(:,tr)=1:ntime;
-        expg_Vreset(:,tr)=exp(-g.*t_elapse(:,tr));
-%         expg_k(:,tr)=cumsum(exp(-g.*t_elapse(:,tr)).*I_eg(:,tr));
+        t_elapse=1:ntime;
+        expg_Vreset(:,tr)=exp(-g.*t_elapse)+g.*cumsum(exp(-g.*t_elapse));
          for t=1:ntime
              expg_k(t,tr)=exp(-g.*(t-[0:t-1]))*I_eg(1:t,tr);
          end
@@ -17,9 +16,8 @@ for tr=1:ntrial
         te0=find(trainM(:,tr));
         te1=[0;te0;ntime];
         for i=1:length(te0)+1
-            t_elapse(te1(i)+1:te1(i+1),tr)=1:te1(i+1)-te1(i);
-            expg_Vreset(te1(i)+1:te1(i+1),tr)=exp(-g.*t_elapse(te1(i)+1:te1(i+1),tr));
-%             expg_k(te1(i)+1:te1(i+1),tr)=cumsum(exp(-g.*t_elapse(te1(i)+1:te1(i+1),tr)).*I_eg(te1(i)+1:te1(i+1),tr));
+            t_elapse=1:te1(i+1)-te1(i);
+            expg_Vreset(te1(i)+1:te1(i+1),tr)=exp(-g.*t_elapse)+g.*cumsum(exp(-g.*t_elapse));
             for t=te1(i)+1:te1(i+1)
                 expg_k(t,tr)=exp(-g.*(t-[te1(i):t-1]))*I_eg(te1(i)+1:t,tr);
             end
