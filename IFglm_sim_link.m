cc52f6862d95a1a4dff9a82c4204b2e1995f4163
shababo <benshababo@gmail.com>
2016-09-07 13:27:03 -0700

    double spiking bug fixed

--- IFglm_sim_link.m
+++ IFglm_sim_link.m
@@ -23,6 +23,7 @@ I_Stim_vect=1.39:0.04:1.59; %magnitudes of pulse of injected current [nA]
 spTrain=zeros(t_end,length(I_Stim_vect));
 
 for I_Stim=I_Stim_vect; %loop over different I_Stim values
+    
     PlotNum=PlotNum+1;
     i=1; %index denoting which element of V is being assigned
         
@@ -31,35 +32,40 @@ for I_Stim=I_Stim_vect; %loop over different I_Stim values
     I_e_vect=zeros(1,t_StimStart/dt); %portion of I_e_vect from t=0 to t_StimStart
     I_e_vect=[I_e_vect I_Stim*ones(1,1+((t_StimEnd-t_StimStart)/dt))];
     I_e_vect=[I_e_vect zeros(1,(t_end-t_StimEnd)/dt)];
-    I_e_vect=awgn(I_e_vect,10,'measured');
+%     I_e_vect=awgn(I_e_vect,10,'measured');
     I_e_vect_mat(:,PlotNum)=I_e_vect;
     
     NumSpikes=0; %holds number of spikes that have occurred
     
     tao=exprnd(1);
     lambda(i)=log(exp(V_vect(i)-V_th)+1);
+    last_spike = 1;
     
     for t=dt:dt:t_end %loop through values of t in steps of df ms        
         %V_inf = E_L + I_e_vect(i)*R_m;
         %V_vect(i+1) = V_inf + (V_vect(i)-V_inf)*exp(-dt/tau);
         
         V_vect(i+1) = V_vect(i) + (E_L-V_vect(i) + I_e_vect(i)*R_m)/tau; %Euler's method
-        
+        lambda(i+1)=log(exp(V_vect(i+1)-V_th)+1);
         
         %if statement below says what to do if voltage crosses threshold
-        if sum(lambda)>tao %cell spiked
+        if sum(lambda(last_spike+1:i+1))>tao %cell spiked
             
             V_vect(i+1)=V_reset; %set voltage back to V_reset
             V_plot_vect(i+1)=V_spike; %set vector that will be plotted to show a spike here
             NumSpikes=NumSpikes+1; %add 1 to the total spike count
-            spTrain(t,PlotNum)=1;
-            
+            spTrain(i+1,PlotNum)=1;
+            if last_spike == i
+                disp('bink')
+                return
+            end
+            last_spike = i+1;
             tao=exprnd(1);
-            lambda(1:i)=0;
-            lambda(i+1)=log(exp(V_vect(i+1)-V_th)+1);
+%             lambda(1:i)=0;
+%             lambda(i+1)=log(exp(V_vect(i+1)-V_th)+1);
         else %voltage didn't cross threshold so cell does not spike
             V_plot_vect(i+1)=V_vect(i+1); %plot actual voltage
-            lambda(i+1)=log(exp(V_vect(i+1)-V_th)+1);
+%             lambda(i+1)=log(exp(V_vect(i+1)-V_th)+1);
         end
         i=i+1; %add 1 to index,corresponding to moving forward 1 time step
     end    
@@ -69,6 +75,8 @@ for I_Stim=I_Stim_vect; %loop over different I_Stim values
     figure(2)
     subplot(length(I_Stim_vect),1,PlotNum)
     plot(t_vect,V_plot_vect);
+    hold on
+    plot(t_vect,lambda*100)
     if (PlotNum==1)
         title('Voltage vs. time');
     end
@@ -77,8 +85,11 @@ for I_Stim=I_Stim_vect; %loop over different I_Stim values
     end
     ylabel('Voltage (mV)');
     
-    clear lambda
-    V_vect=zeros(1,length(t_vect));
+    if I_Stim ~= I_Stim_vect(end)
+        clear lambda
+        V_vect=zeros(1,length(t_vect));
+        V_plot_vect=zeros(1,length(t_vect));
+    end
 end
 
 
