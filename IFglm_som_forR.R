380b151c55c6764ade3534530e9430bf7795a065
xin-yi <xinyi@math.bu.edu>
2016-11-30 15:40:38 -0500

    glmfit som data R part

---
+++ IFglm_som_forR.R
@@ -0,0 +1,18 @@
+library(R.matlab)
+library(nnls)
+library(glmnet)
+setwd("~/Research/Fall 2016 circuit");
+
+# dataMat<-readMat('forR_som_cell12loc1all.mat');
+dataMat<-readMat('test.mat');
+design1<-dataMat$design1;
+design2<-dataMat$design2;
+Y<-dataMat$Y;
+
+########################
+net.fit<-glmnet(cbind(design1[,2]*-1,design2),Y,lower=0,family='poisson');
+betafit<-coef(net.fit,0);
+
+betafit
+
+write(betafit[,1], file = "betafit.txt", sep = " ");
